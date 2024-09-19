05258cede07d3faa8765acba86e34925b49ff4cc
kinyonyi <kinyonyidavid@gmail.com>
2024-09-19 19:52:38 +0300

    comments and how to create and run a dart application

---
+++ myPracticeDart/intro.dart
@@ -0,0 +1,12 @@
+//defines a single line comment 
+
+/*
+This defines multi line comment in dart programming
+can span multiple lines 
+Dart is an object oriented programming language 
+*/
+
+//main function, the entry point to a dart program
+void main() {
+  print('hello world');
+}
